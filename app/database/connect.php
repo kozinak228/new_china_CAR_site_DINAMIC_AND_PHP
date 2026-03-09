6a63bf8eb91a19539ac7dca7d9b152b51c8ff70a
kozinak228 <kozinak228@github.com>
2026-03-09 19:27:50 +0700

    feat: environment-aware database and path configuration

--- app/database/connect.php
+++ app/database/connect.php
@@ -2,9 +2,18 @@
 
 $driver = 'mysql';
 $host = 'localhost';
-$db_name = 'dinamic-site';
-$db_user = 'root';
-$db_pass = '';
+$is_local = in_array($_SERVER['HTTP_HOST'] ?? '', ['localhost', 'localhost:8000', '127.0.0.1']);
+
+if ($is_local) {
+    $db_name = 'dinamic-site';
+    $db_user = 'root';
+    $db_pass = '';
+}
+else {
+    $db_name = 'u3414051_default';
+    $db_user = 'u3414051_default';
+    $db_pass = 'wJ7u8AHiO3szaTZ1';
+}
 $charset = 'utf8mb4';
 $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
--- path.php
+++ path.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined('SITE_ROOT')) {
     define('SITE_ROOT', __DIR__);
-    define('BASE_URL', "http://localhost:8000/");
+    $is_local = in_array($_SERVER['HTTP_HOST'] ?? '', ['localhost', 'localhost:8000', '127.0.0.1']);
+    define('BASE_URL', $is_local ? "http://localhost:8000/" : "https://avtotachka.ru/");
     define('ROOT_PATH', realpath(dirname(__FILE__)));
 }
